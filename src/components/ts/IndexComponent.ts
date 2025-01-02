36cf7abcda810a383437016bbfdb7555c807108e
unknown <you@example.com>
2025-01-02 18:03:17 +0330

    first commit

---
+++ src/components/ts/IndexComponent.ts
@@ -0,0 +1,40 @@
+import { ref } from 'vue';
+
+const posts = ref([
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Paniz',
+    description: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available',
+    upVoteCount: 21,
+  },
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Paniz',
+    description: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available',
+    upVoteCount: 10,
+  },
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Paniz',
+    description: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available',
+    upVoteCount: 23,
+  },
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Paniz',
+    description: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available',
+    upVoteCount: 9,
+  },
+]);
+
+export {posts}
+
+
